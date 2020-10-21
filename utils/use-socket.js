c92048ad00237ee6c4805f5221ddc742629b5f94
Andrés Felipe Martínez Salazar <andresfelipe.2031@gmail.com>
2020-10-20 20:17:07 -0500

    prod socket server

--- utils/use-socket.js
+++ utils/use-socket.js
@@ -1,22 +1,11 @@
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-process.env.SERVER_HOST = "kuronosu.dev";
-process.env.SERVER_PORT = "default";
-
 export default function useSocket(stop = []) {
   const [s, set] = useState(null);
   useEffect(() => {
     // fetch("/api/socketio").finally(() => set(io()));
-    set(
-      io(
-        `ws://${process.env.SERVER_HOST || "127.0.0.1"}${
-          process.env.SERVER_PORT == "default"
-            ? ""
-            : ":" + (process.env.SERVER_PORT || 3001)
-        }`
-      )
-    );
+    set(io(`https://kuronosu.dev`));
   }, stop);
   return s;
 }
